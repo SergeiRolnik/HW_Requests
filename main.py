1ef53c0230af7cb8e6b9e7b9d68d9f6c2d94a9f7
Сергей Рольник <forgedwheels@gmail.com>
2022-01-06 17:05:26 +0300

    New features developed

--- main.py
+++ main.py
@@ -1,16 +1,2 @@
-# Задача №3
-queries = [
-    'смотреть сериалы онлайн',
-    'новости спорта',
-    'афиша кино',
-    'курс доллара',
-    'сериалы этим летом',
-    'курс по питону',
-    'сериалы про спорт'
-    ]
-freqs = {}  # словарь где ключом будет количество слов в запросе, значением - количество таких запросов
-for query in queries:
-    word_count = len(query.split())
-    freqs[word_count] = freqs.get(word_count, 0) + 1
-for key, value in sorted(freqs.items()):
-    print(f'Поисковых запросов из {key} слов - {int(value / sum(freqs.values()) * 100)}%')
+print("V 1.0")
+print("New features developed")
